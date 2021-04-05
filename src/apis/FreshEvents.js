03bfbd5a026286012071f1294924aac5fe5e6758
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2021-04-05 12:22:35 +0200

    url change

--- src/apis/FreshEvents.js
+++ src/apis/FreshEvents.js
@@ -26,7 +26,7 @@ class FreshEvents extends Component {
             var options = {
                 method: 'GET',
                 //url: 'http://localhost:5000/api/getdata',
-                url: 'https://tippmix-backend.herokuapp.com/api/getdata',
+                url: 'https://tippmix-backend.herokuapp.com/getdata',
                 header: {
                     'Accept-Language': 'en-US,en;q=0.9,hu;q=0.8,de;q=0.7',
                     'Content-Type': 'application/json',
