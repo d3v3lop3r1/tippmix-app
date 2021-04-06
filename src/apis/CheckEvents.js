b2c556ca2fe6e6407b3adb05243bdba0a24d6900
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2021-04-06 10:02:34 +0200

    link for heroku

--- src/apis/CheckEvents.js
+++ src/apis/CheckEvents.js
@@ -16,7 +16,8 @@ class CheckEvents extends Component {
 
     getResult = async (participants)=>{
         try {
-            const results = await axios.get(`http://localhost:5000/api/getresult/${participants}`);
+            //const results = await axios.get(`http://localhost:5000/api/getresult/${participants}`);
+            const results = await axios.get(`https://tippmix-backend.herokuapp.com/api/getresult/${participants}`);
             console.log(results);
             return results;
         } catch (error) {
